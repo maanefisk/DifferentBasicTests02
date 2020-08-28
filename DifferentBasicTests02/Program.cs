9e05cad2537dc3324377e58c4bc01bafa86e5700
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-08-28 13:38:36 +0200

    The average

--- DifferentBasicTests02/Program.cs
+++ DifferentBasicTests02/Program.cs
@@ -6,6 +6,20 @@ namespace DifferentBasicTests02
     {
         static void Main(string[] args)
         {
+            //Test2
+            Console.Write("\n First number: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n Second number: ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n Third number: ");
+            int third = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n Fourth number: ");
+            int fourth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n" + "The average of " + first + " & " + second + " & " + third + " & " + fourth + " = " +
+                              ((first + second + third + fourth)/4) + "\n");
+
+            //Test1
             Console.WriteLine("Enter a number you like to see multiplied: ");
             int numbr1 = Convert.ToInt32(Console.ReadLine());
             int x = 11;
