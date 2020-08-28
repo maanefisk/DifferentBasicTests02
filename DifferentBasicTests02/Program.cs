0e45cd42b58d5c5e7913bc68b9f69017f44a7e4d
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-08-28 12:49:00 +0200

    Multiplication

--- DifferentBasicTests02/Program.cs
+++ DifferentBasicTests02/Program.cs
@@ -6,7 +6,14 @@ namespace DifferentBasicTests02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a number you like to see multiplied: ");
+            int numbr1 = Convert.ToInt32(Console.ReadLine());
+            int x = 11;
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine("\n"+numbr1 + " x " + i + " = " + (numbr1*i));
+            }
         }
     }
 }
